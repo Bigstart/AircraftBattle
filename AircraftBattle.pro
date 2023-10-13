a5fcc34245725c84822b14e0afcad0e383ae0215
Bigstart <1715755221@qq.com>
2023-10-13 16:41:47 +0800

    完善了子弹类

--- AircraftBattle.pro
+++ AircraftBattle.pro
@@ -10,6 +10,7 @@ CONFIG += c++17
 
 SOURCES += \
     bullet.cpp \
+    enemyplane.cpp \
     heroplane.cpp \
     main.cpp \
     mainscene.cpp \
@@ -18,6 +19,7 @@ SOURCES += \
 HEADERS += \
     bullet.h \
     config.h \
+    enemyplane.h \
     heroplane.h \
     mainscene.h \
     map.h
--- config.h
+++ config.h
@@ -20,6 +20,12 @@
 #define BULLET_PATH ":/res/bullet_11.png"   //子弹图片路径
 #define BULLET_SPEED 5    //子弹移动速度
 #define BULLET_NUM 30    //弹匣中子弹总数
-#define BULLET_INTERVAL 20  //发射子弹时间间隔
+#define BULLET_INTERVAL 25  //发射子弹时间间隔
+
+//------------敌机配置数据-----------------------
+#define ENEMY_PATH ":/res/img-plane_5.png"   //敌机资源图片
+#define ENEMY_SPEED 5   //敌机移动速度
+#define ENEMY_NUM  20   //敌机总数量
+#define ENEMY_INTERVAL  //敌机出场时间间隔
 
 #endif // CONFIG_H
